ca2d637a07c9c7b4edb31da6cbca67b4ef3cf30b
januadrym <mquandlvn@gmail.com>
2020-05-26 10:29:38 +0700

    authen-fix

--- internal/app/auth/handler.go
+++ internal/app/auth/handler.go
@@ -1,14 +1,18 @@
 package auth
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 
+	"github.com/anmotor/internal/app/types"
 	"github.com/anmotor/internal/pkg/http/respond"
+	"github.com/sirupsen/logrus"
 )
 
 type (
 	service interface {
+		Auth(ctx context.Context, username, password string) (string, *types.User, error)
 	}
 
 	Handler struct {
@@ -32,4 +36,16 @@ func (h *Handler) Auth(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer r.Body.Close()
+	token, user, err := h.srv.Auth(r.Context(), req.Username, req.Password)
+	if err != nil {
+		logrus.Errorf("unauthorized, err: %v", http.StatusUnauthorized)
+		respond.Error(w, err, http.StatusUnauthorized)
+		return
+	}
+	respond.JSON(w, http.StatusOK, types.BaseResponse{
+		Data: map[string]interface{}{
+			"token":     token,
+			"user_info": user,
+		},
+	})
 }
